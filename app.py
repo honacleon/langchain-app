c35fd8d8a5befd8a1d17e10b676a679eb47e9773
Honacleon Junior <hpouro@outlook.com>
2025-01-11 09:37:57 -0300

    Atualização do Projeto, agora funciona!

--- app.py
+++ app.py
@@ -6,29 +6,60 @@ from PIL import Image
 import docx
 import streamlit as st
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from pypdf.errors import PdfReadError
-from openai.error import AuthenticationError, InvalidRequestError
+from openai import AuthenticationError, BadRequestError
+
+
+
+
+# Alterar o estilo da página com CSS
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #1A1A1D;
+        color: #A64D79;
+    }
+    .stButton > button {
+        background-color: #FF204E;
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Adicionar o título personalizado com HTML
+
+
+st.markdown(
+    """
+    <h1 style='text-align: center; color:white;'>
+        By: <span style='color: white;'>Honacleon Junior</span>
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
 
 # Adicionar a imagem no cabeçalho
-image_url = "https://cienciadosdados.com/images/CINCIA_DOS_DADOS_4.png"
-st.image(image_url, use_column_width=True)
+# image_url = "https://cienciadosdados.com/images/CINCIA_DOS_DADOS_4.png"
+# st.image(image_url, use_container_width=True)
 
 # Adicionar o nome do aplicativo
-st.subheader("Q&A com IA - PLN usando LangChain")
+st.subheader("Perguntas e Respostas com IA - PLN usando LangChain")
 
 # Componentes interativos
-file_input = st.file_uploader("Upload a file", type=['pdf', 'txt', 'csv', 'docx', 'jpeg', 'png'])
-openaikey = st.text_input("Enter your OpenAI API Key", type='password')
-prompt = st.text_area("Enter your questions", height=160)
-run_button = st.button("Run!")
+file_input = st.file_uploader("Faça upload de um arquivo", type=['pdf', 'txt', 'csv', 'docx', 'jpeg', 'png'])
+openaikey = st.text_input("Digite sua chave de API da OpenAI", type='password')
+prompt = st.text_area("Digite suas perguntas", height=160)
+run_button = st.button("Executar!")
 
-select_k = st.slider("Number of relevant chunks", min_value=1, max_value=5, value=2)
-select_chain_type = st.radio("Chain type", ['stuff', 'map_reduce', "refine", "map_rerank"])
+select_k = st.slider("Número de trechos relevantes", min_value=1, max_value=5, value=2)
+select_chain_type = st.radio("Tipo de cadeia", ['stuff', 'map_reduce', "refine", "map_rerank"])
 
 # Função para carregar documentos
 def load_document(file_path, file_type):
@@ -51,7 +82,7 @@ def load_document(file_path, file_type):
         text = pytesseract.image_to_string(Image.open(file_path))
         return [{"page_content": text}]
     else:
-        st.error("Unsupported file type.")
+        st.error("Tipo de arquivo não suportado.")
         return None
 
 # Função de perguntas e respostas
@@ -61,51 +92,47 @@ def qa(file_path, file_type, query, chain_type, k):
         if not documents:
             return None
         
-        # split the documents into chunks
+        # Dividir os documentos em trechos
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_documents(documents)
         
-        # select which embeddings we want to use
+        # Selecionar quais embeddings usar
         embeddings = OpenAIEmbeddings()
         
-        # create the vectorestore to use as the index
+        # Criar o banco de vetores para usar como índice
         db = Chroma.from_documents(texts, embeddings)
         
-        # expose this index in a retriever interface
+        # Expor esse índice em uma interface de recuperação
         retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
         
-        # create a chain to answer questions 
+        # Criar uma cadeia para responder às perguntas
         qa = RetrievalQA.from_chain_type(
-            llm=ChatOpenAI(model="gpt-4"), 
+            llm=ChatOpenAI(model="gpt-4o-mini"), 
             chain_type=chain_type, 
             retriever=retriever, 
             return_source_documents=True
         )
-        result = qa({"query": query})
+        result = qa.run(query)
         return result
     except PdfReadError as e:
-        st.error(f"Error reading PDF file: {e}")
+        st.error(f"Erro ao ler o arquivo PDF: {e}")
         return None
     except AuthenticationError as e:
-        st.error(f"Authentication error: {e}")
+        st.error(f"Erro de autenticação: {e}")
         return None
-    except InvalidRequestError as e:
-        st.error(f"Invalid request error: {e}")
+    except BadRequestError as e:
+        st.error(f"Erro de requisição inválida: {e}")
         return None
 
 # Função para exibir o resultado no Streamlit
 def display_result(result):
     if result:
-        st.markdown("### Result:")
-        st.write(result["result"])
-        st.markdown("### Relevant source text:")
-        for doc in result["source_documents"]:
-            st.markdown("---")
-            st.markdown(doc.page_content)
+        st.markdown("### Resultado:")
+        st.write(result)
 
 # Execução do app
 if run_button and file_input and openaikey and prompt:
-    with st.spinner("Running QA..."):
+    with st.spinner("Executando QA..."):
         # Salvar o arquivo em um local temporário
         temp_file_path = os.path.join(tempfile.gettempdir(), file_input.name)
         with open(temp_file_path, "wb") as temp_file:
@@ -120,7 +147,7 @@ if run_button and file_input and openaikey and prompt:
             embeddings = OpenAIEmbeddings()
             embeddings.embed_documents(["test"])
         except AuthenticationError as e:
-            st.error(f"Invalid OpenAI API Key: {e}")
+            st.error(f"Chave de API da OpenAI inválida: {e}")
         else:
             # Executar a função de perguntas e respostas
             result = qa(temp_file_path, file_input.type, prompt, select_chain_type, select_k)
